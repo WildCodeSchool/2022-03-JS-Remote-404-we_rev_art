c015ac20f87746eacb263a0afea5df3c2fce7885
Mariakarara <claind15@yahoo.fr>
2022-05-30 23:06:35 +0200

    travail css suggestioncard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
+import Suggestioncard from "@components/Suggestioncard";
 import Hero from "../components/Hero";
 import TitleText from "../components/TitleText";
 import titleTextData from "../data/titleTextData";
+import suggestionsData from "../data/suggestionsData";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <TitleText titleTextData={titleTextData} />
+      <Suggestioncard suggestionsData={suggestionsData} />
     </div>
   );
 }
