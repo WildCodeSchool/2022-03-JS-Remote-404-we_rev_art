33d0988ef4e6304e1bae8b5968c74633d32a78df
Terry Grimoire <terrt.grimoire@gmail.com>
2022-05-30 15:24:31 +0200

    deleting nav from component home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import Nav from "../components/Nav";
 import Hero from "../components/Hero";
 import TitleText from "../components/TitleText";
 import titleTextData from "../data/titleTextData";
@@ -6,7 +5,6 @@ import titleTextData from "../data/titleTextData";
 export default function Home() {
   return (
     <div>
-      <Nav />
       <Hero />
       <TitleText titleTextData={titleTextData} />
     </div>
