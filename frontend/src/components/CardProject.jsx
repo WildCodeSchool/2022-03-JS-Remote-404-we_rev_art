4ae723732fd51722c686f3ef9320da34bdbd18d0
Quentin Billac <quentin.billac2@gmail.com>
2022-05-31 18:31:43 +0200

    ajout du link sur le bouton

--- frontend/src/components/CardProject.jsx
+++ frontend/src/components/CardProject.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import lastProject from "../data/lastProject";
 import "../style/cardProject.css";
 
@@ -34,9 +35,11 @@ function CardProject() {
         </div>
       ))}
       <div className="homeToAllCard">
-        <button className="allCard" type="button">
-          VIEW ALL ADS
-        </button>
+        <Link to="/ProjectAds">
+          <button className="allCard" type="button">
+            VIEW ALL ADS
+          </button>
+        </Link>
       </div>
     </div>
   );
