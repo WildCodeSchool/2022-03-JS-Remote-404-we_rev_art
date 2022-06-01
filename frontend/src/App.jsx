413f8ce85dc0441db6e9398b370024987d2f9316
EtienneApi <93768053+EtienneApi@users.noreply.github.com>
2022-06-01 11:23:05 +0200

    hotfix css navbar and grid suggestionCard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,6 @@ import Login from "./pages/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-
 import "./App.css";
 
 function App() {
--- frontend/src/components/Suggestioncard.jsx
+++ frontend/src/components/Suggestioncard.jsx
@@ -4,14 +4,14 @@ import "../style/SuggestionCard.css";
 function Suggestioncard({ suggestionsData }) {
   return (
     <div className="suggestioncard-container">
-      <h2 className="suggestion_h2">
-        What would you like to do with your art?
-      </h2>
-      {suggestionsData.map((data) => (
-        <section key={data.id} className="sectionsuggestion">
-          <p className="type_container">{data.type}</p>
-        </section>
-      ))}
+      <h2>What would you like to do with your art?</h2>
+      <div className="cardsuggestion">
+        {suggestionsData.map((data) => (
+          <section key={data.id} className="sectionsuggestion">
+            <p className="type_container">{data.type}</p>
+          </section>
+        ))}
+      </div>
     </div>
   );
 }
