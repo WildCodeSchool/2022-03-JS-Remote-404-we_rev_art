567c7fb5125b8896507e7ec85ebb43c8be9bd117
Terry Grimoire <terrt.grimoire@gmail.com>
2022-06-06 22:08:41 +0200

    adding css to the section

--- frontend/src/components/Field.jsx
+++ frontend/src/components/Field.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import fieldDataArtistAccount from "../data/fieldDataArtistAccount";
+import "../style/Field.css";
 
 function Field() {
   return (
     <div className="field_container">
       {fieldDataArtistAccount.map((el) => (
         <label htmlFor={el.label} className="field_label">
-          {el.label} {el.required === true ? <span> {el.required} </span> : ""}{" "}
-          <input type="text" />
+          <div>
+            {el.label}
+            {el.required ? (
+              <span className="field_span"> (required) </span>
+            ) : (
+              ""
+            )}
+          </div>
+          <input
+            type="text"
+            className="field_input"
+            placeholder={`Enter your ${el.label.toLowerCase()}`}
+          />
         </label>
       ))}
     </div>
