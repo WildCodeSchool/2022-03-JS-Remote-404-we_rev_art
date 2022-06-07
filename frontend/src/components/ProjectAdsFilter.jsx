5f065fb6e739c2cf224b7a307b5c3f32bbd05cdf
Mariakarara <claind15@yahoo.fr>
2022-06-07 17:03:10 +0200

    correction bug projectadsfilter

--- frontend/src/components/ProjectAdsFilter.jsx
+++ frontend/src/components/ProjectAdsFilter.jsx
@@ -1,14 +1,9 @@
-import { React, useState } from "react";
+import { React } from "react";
 import ProjectAdsFilterData from "../data/ProjectAdsFilterData";
 import "../style/ProjectAdsFilter.css";
 import ProjectFilterOption from "./ProjectFilterOption";
 
 function ProjectAdsFilter() {
-  const [buttonClicked, setButtonClicked] = useState(false);
-  function handleClick() {
-    setButtonClicked(!buttonClicked);
-  }
-
   return (
     <div className="projectad_container">
       <h2> Project Ads</h2>
@@ -17,23 +12,8 @@ function ProjectAdsFilter() {
         artists, illustrators, museums, galleries, collectors and art lovers who
         are looking for digital artist(s) to animate physical artwork(s)
       </h3>
-
       {ProjectAdsFilterData.map((data) => (
-        <section className="filter_button_container">
-          <button
-            className="filter_selection"
-            type="button"
-            onClick={handleClick}
-          >
-            {data.filter}
-          </button>
-
-          <ul>
-            {buttonClicked
-              ? data.option.map((list) => <ProjectFilterOption list={list} />)
-              : ""}
-          </ul>
-        </section>
+        <ProjectFilterOption list={data} />
       ))}
     </div>
   );
--- frontend/src/components/ProjectFilterOption.jsx
+++ frontend/src/components/ProjectFilterOption.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 
 function ProjectFilterOption({ list }) {
+  const [buttonClicked, setButtonClicked] = useState(false);
+  function handleClick() {
+    setButtonClicked(!buttonClicked);
+  }
   return (
-    <Link to>
-      <li className="filter_options"> {list}</li>{" "}
-    </Link>
+    <section className="XXXX">
+      <button className="filter_selection" type="button" onClick={handleClick}>
+        {list.filter}
+      </button>
+
+      <ul>
+        {buttonClicked
+          ? list.option.map((filteroptions) => (
+              <Link to="/">
+                <li className="filter_options"> {filteroptions}</li>
+              </Link>
+            ))
+          : ""}
+      </ul>
+    </section>
   );
 }
 
