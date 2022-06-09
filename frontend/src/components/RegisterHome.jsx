8a2ab0396174a8fdf6677c00e8500c73621e6f35
EtienneApi <93768053+EtienneApi@users.noreply.github.com>
2022-06-09 17:32:45 +0200

    mise en place de bootstrap

--- frontend/src/components/RegisterHome.jsx
+++ frontend/src/components/RegisterHome.jsx
@@ -6,21 +6,21 @@ import "../style/cardProject.css";
 
 function RegisterHome() {
   return (
-    <div className="registerAlignement">
-      <div className="registerHome">
+    <div className="cardProject_registerAlignement">
+      <div className="cardProject_registerHome">
         <h3>ARE YOU A DIGITAL ARTIST ?</h3>
         <br />
         <p>register for free to find animation offers</p>
         <br />
         <Link to="/ProjectAds">
-          <button className="registerButton" type="button">
+          <button className="cardProject_registerButton" type="button">
             CREATE YOUR PORTFOLIO
           </button>
         </Link>
         <br />
         <br />
         <h3>WHY REGISTER ?</h3>
-        <div className="checklist">
+        <div className="cardProject_checklist">
           <Checklist index={3} />
         </div>
       </div>
