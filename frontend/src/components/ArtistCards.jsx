e05c9205602cf4b207dde49cc94950e21f54b3bb
Mariakarara <claind15@yahoo.fr>
2022-06-13 17:19:10 +0200

    no changes to previous add

--- frontend/src/components/ArtistCards.jsx
+++ frontend/src/components/ArtistCards.jsx
@@ -4,23 +4,21 @@ import "../style/ArtistCard.css";
 
 function ArtistCards() {
   return (
-    <section className="card">
+    <section className="xxx">
       <div className="card_map">
         {ArtistCardData.map((data) => (
-          <div className="allcards" key={data.id}>
+          <div className="card" key={data.id}>
+            <img className="card-img-top" src={data.image} alt={data.alt} />
             <div>
-              <img className="card-img-top" src={data.image} alt={data.alt} />
-              <div>
-                <h4 className="card-title">{data.username}</h4>
-                <p className="card-titlebis">
-                  {data.country} <br />
-                  {data.nbcreation}
-                </p>
-              </div>
+              <h4 className="card-title">{data.username}</h4>
+              <p className="card-titlebis">
+                {data.country} <br />
+                {data.nbcreation}
+              </p>
               <ul className="list-group list-group-flush">
-                <li className="list-group-item1"> {data.technique}</li>
-                <li className="list-group-item2"> {data.service_provider}</li>
-                <li className="list-group-item3"> {data.type_of_contract}</li>
+                <li className="list-group-item"> {data.technique}</li>
+                <li className="list-group-item"> {data.service_provider}</li>
+                <li className="list-group-item"> {data.type_of_contract}</li>
               </ul>
               <br />
               <p className="card-body"> Details of ad : {data.presentation}</p>
