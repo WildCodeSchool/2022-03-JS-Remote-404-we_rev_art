216e5e6168ac0add92801608818d1dbde09b7e0c
Mariakarara <claind15@yahoo.fr>
2022-06-15 17:20:23 +0200

    end of work on artist card and like button usestate

--- frontend/src/components/HelpInfo.jsx
+++ frontend/src/components/HelpInfo.jsx
@@ -22,13 +22,30 @@ function HelpInfo() {
       }, 10000);
     }
   }, [rows]);
+  const styles = {
+    // bgColor: 'white',
+    // titleTextColor: "blue",
+    // rowContentColor: 'grey',
+    arrowColor: "#E5735C",
+  };
+
+  const config = {
+    // animate: true,
+    // arrowIcon: "V",
+    // tabFocus: true
+  };
 
   return (
     <div>
-      <h3 className="section-title">FAQ (how it works?)</h3>
+      <h3 className="section-title">General FAQ </h3>
 
       <div>
-        <Faq data={data} getRowOptions={setRowsOption} />
+        <Faq
+          data={data}
+          styles={styles}
+          config={config}
+          getRowOptions={setRowsOption}
+        />
       </div>
     </div>
   );
