30326526c947714246c5b955066b8366ade2a469
Quentin Billac <quentin.billac2@gmail.com>
2022-07-05 14:21:39 +0200

    initialisation des filtres dynamique

--- frontend/src/components/Filters/ProjectAds/ProjectAdsFilter.jsx
+++ frontend/src/components/Filters/ProjectAds/ProjectAdsFilter.jsx
@@ -1,25 +1,9 @@
-import axios from "axios";
-import { React, useState, useEffect } from "react";
+import { React } from "react";
 import ProjectAdsFilterData from "../../../data/ProjectAdsFilterData";
 import "../../../style/ProjectAdsFilter.css";
 import ProjectFilterOption from "../ProjectFilterOption";
 
 function ProjectAdsFilter({ filter, setFilter }) {
-  const [filtersOption, setFiltersOption] = useState([]);
-  const getFilters = async () => {
-    const provisoirFilter = await Promise.all(
-      ProjectAdsFilterData.map((filterdata) => {
-        return axios
-          .get(`${import.meta.env.VITE_BACKEND_URL}/filters/${filterdata.type}`)
-          .then((res) => res.data);
-      })
-    );
-    setFiltersOption(provisoirFilter);
-    console.warn(filtersOption);
-  };
-  useEffect(() => {
-    getFilters();
-  }, []);
   return (
     <div className="projectad_container">
       <div className="buttonlist_map">
